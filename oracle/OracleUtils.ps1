f6a9d1e721fe6e234f39fc55b69eb1550a9e11c3
Jesus Sanchez <jsanchez.consultant@gmail.com>
2017-11-08 15:43:00 -0600

    Update OracleUtils.ps1

--- oracle/OracleUtils.ps1
+++ oracle/OracleUtils.ps1
@@ -80,14 +80,15 @@ function Ping-OracleDB
 .Synopsis
     This will run a SQL script on one or more Oracle databases by leveraging SQL*Plus
 .DESCRIPTION
-    This functions runs a SQL script on a Oracle Database and returns the output from the script
+    This function runs a SQL script on a Oracle Database and returns the output from the script
 .EXAMPLE
-    Run-OracleScript -TargetDB <DB NAME> -SQLScript <Path/to/file.sql>
-    .ROLE
-       This cmdlet is mean to be used by Oracle DBAs
+    Run-OracleScript -TargetDB <DB NAME> -SQLScript <Path/to/file.sql> [-ErrorLog]
+.ROLE
+    This cmdlet is mean to be used by Oracle DBAs
 #>
 function Run-OracleScript {
-    [CmdletBinding()]
+    [CmdletBinding(
+        SupportsShouldProcess=$true)]
     [Alias("rsql")]
     Param (
         # It can run the script on several databases at once
@@ -126,17 +127,66 @@ function Run-OracleScript {
 
 <#
 .Synopsis
+    Query an Oracle database to get the global name
+.DESCRIPTION
+    This function returns the global name of the database
+.EXAMPLE
+    Get-OracleGlobalName -TargetDB <DB NAME> [-ErrorLog]
+    .ROLE
+       This cmdlet is mean to be used by Oracle DBAs
+#>
+function Get-OracleGlobalName {
+    [CmdletBinding()]
+    [Alias("gogn")]
+    Param (
+        # This can be a list of databases
+        [Parameter(Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            HelpMessage="One or more Oracle Database names")]
+        [String[]]$TargetDB,
+
+        # Switch to turn on the error logging
+        [Switch]$ErrorLog,
+        [String]$ErrorLogFile = "$env:TEMP\OracleUtils_Errors_$PID.log"
+    )
+    Begin{}
+    Process{
+        if (Check-OracleEnv) {
+            foreach ($db in $TargetDB) {
+                if (podb($db)) {
+                    Write-Output "Database $db is reachable"
+                    # Using here-string to pipe the SQL query to SQL*Plus
+                    @'
+SET HEADING OFF
+SET PAGESIZE 0
+SELECT global_name from global_name;
+exit
+'@ | &"sqlplus" "-S" "/@$db"
+                } else {
+                    Write-Error "Database $db not reachable" >> $ErrorLogFile
+                }
+            }
+        } else {
+            Write-Error "No Oracle Home detected, please install at least the Oracle Client and try again"
+        }
+    }
+    End{}
+}
+
+<#
+.Synopsis
     Query an Oracle database to get the names of all its instances
 .DESCRIPTION
-    This functions runs a SQL script on a Oracle Database and returns the output from the script
+    This function returns instance names for a target oracle database
 .EXAMPLE
-    Run-OracleScript -TargetDB <DB NAME> -SQLScript <Path/to/file.sql>
+    Get-OracleInstance -TargetDB <DB NAME> -SQLScript <Path/to/file.sql> [-ErrorLog]
     .ROLE
        This cmdlet is mean to be used by Oracle DBAs
 #>
 function Get-OracleInstances {
     [CmdletBinding()]
-    [Alias("gdbi")]
+    [Alias("goin")]
     Param (
         # This can be a list of databases
         [Parameter(Mandatory=$true,
@@ -163,11 +213,127 @@ SELECT instance_name from gv$instance ORDER BY 1;
 exit
 '@ | &"sqlplus" "-S" "/@$db"
                 } else {
-                    Write-Error "Database $db not reachable" >> $ErrorLogFile
+                    Write-Error "Database $db not reachable" | Out-File $ErrorLogFile
                 }
             }
         } else {
             Write-Error "No Oracle Home detected, please install at least the Oracle Client and try again"
+        }
+    }
+    End{}
+}
+
+<#
+.Synopsis
+    
+.DESCRIPTION
+    
+.EXAMPLE
+    
+.ROLE
+    
+#>
+function Get-OracleSnapshots {
+    [CmdletBinding()]
+    [Alias("gos")]
+    Param (
+        # This can be a list of databases
+        [Parameter(Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            HelpMessage="One or more Oracle Database names")]
+        [String[]]$TargetDB,
+
+        # This is the starting time for the permoance snapshot
+        [Parameter(Mandatory=$true,
+            HelpMessage="Starting snapshot approximate time")]
+        [datetime]$StartTime,
+        # This is the ending time for the permoance snapshot
+        [Parameter(Mandatory=$true,
+            HelpMessage="Ending snapshot approximate time")]
+        [datetime]$EndTime,
+
+        # Switch to turn on the error logging
+        [Switch]$ErrorLog,
+        [String]$ErrorLogFile = "$env:TEMP\OracleUtils_Errors_$PID.log"
+    )
+    Begin{}
+    Process{
+        if (Check-OracleEnv) {
+            foreach ($db in $TargetDB) {
+                if (podb($db)) {
+                    $StrStartStamp = $StartTime.AddSeconds(59).ToString("yyyy-MM-dd HH:mm:ss")
+                    Write-Debug $StrStartStamp
+                    $StrEndStamp = $EndTime.ToString("yyyy-MM-dd HH:mm:ss")
+                    Write-Debug $StrEndStamp
+                    # Using here-string to pipe the SQL query to SQL*Plus
+                    @"
+SET HEADING OFF
+SET PAGESIZE 0
+SELECT max(snap_id)
+FROM dba_hist_snapshot
+WHERE begin_interval_time <= TO_TIMESTAMP('$StrStartStamp','YYYY-MM-DD HH24:MI:SS')
+AND end_interval_time > TO_TIMESTAMP('$StrStartStamp','YYYY-MM-DD HH24:MI:SS');
+SELECT min(snap_id)
+FROM dba_hist_snapshot
+WHERE end_interval_time >= TO_TIMESTAMP('$StrEndStamp','YYYY-MM-DD HH24:MI:SS')
+AND begin_interval_time < TO_TIMESTAMP('$StrEndStamp','YYYY-MM-DD HH24:MI:SS');"
+EXIT
+"@ | &"sqlplus" "-S" "/@$db"
+                } else {
+                    Write-Error "Database $db not reachable" | Out-File $ErrorLogFile
+                }
+            }
+        } else {
+            Write-Error "No Oracle Home detected, please install at least the Oracle Client and try again"
+        }
+    }
+    End{}
+}
+
+
+
+<#
+.Synopsis
+    Generate AWR and/or ADDM report sets from an oracle database
+.DESCRIPTION
+    This function defines the required variables to generate automatically AWR and ADDM report sets.
+.EXAMPLE
+    Get-OraclePerfReports  [-AWR] [-ADDM] -TargetDB <DB NAME> [-ErrorLog]
+    .ROLE
+       This cmdlet is mean to be used by Oracle DBAs
+#>
+function Get-OraclePerfReport {
+    [CmdletBinding()]
+    [Alias("gopr")]
+    Param (
+        # This can be a list of databases
+        [Parameter(Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            HelpMessage="One or more Oracle Database names")]
+        [String[]]$TargetDB,
+        # This is the starting time for the permoance snapshot
+        [Parameter(Mandatory=$true,
+            HelpMessage="Starting snapshot approximate time")]
+        [datetime]$StartTime,
+        # This is the ending time for the permoance snapshot
+        [Parameter(Mandatory=$true,
+            HelpMessage="Ending snapshot approximate time")]
+        [datetime]$EndTime,
+        # Switch to turn on the error logging
+        [Switch]$ErrorLog,
+        [String]$ErrorLogFile = "$env:TEMP\OracleUtils_Errors_$PID.log"
+    )
+    Begin {}
+    Process {
+        Write-Warning "NOT IMPLEMENTED YET!"
+        if (Check-OracleEnv) {
+            if (Ping-OracleDB -TargetDB $TargetDB) {
+
+                $StartTime.AddSeconds(59).ToString("yyyy-MM-dd HH:mm:ss")
+                $EndTime.ToString("yyyy-MM-dd HH:mm:ss")
+            }
         }
     }
     End{}
