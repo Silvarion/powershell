c43c0ce0c3118bb659af41c21d44904741fe3f4d
Jesus Sanchez <jsanchez.consultant@gmail.com>
2017-11-09 16:26:31 -0600

    Update OracleUtils.ps1

--- oracle/OracleUtils.ps1
+++ oracle/OracleUtils.ps1
@@ -282,7 +282,7 @@ exit
 .ROLE
     
 #>
-function Get-OracleSnapshots {
+function Get-OracleSnapshot {
     [CmdletBinding()]
     [Alias("gos")]
     Param (
@@ -291,16 +291,16 @@ function Get-OracleSnapshots {
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true,
             HelpMessage="One or more Oracle Database names")]
-        [String[]]$TargetDB,
+        [String]$TargetDB,
 
-        # This is the starting time for the permoance snapshot
+        # This is the time for the permoance snapshot
         [Parameter(Mandatory=$true,
-            HelpMessage="Starting snapshot approximate time")]
-        [datetime]$StartTime,
-        # This is the ending time for the permoance snapshot
-        [Parameter(Mandatory=$true,
-            HelpMessage="Ending snapshot approximate time")]
-        [datetime]$EndTime,
+            HelpMessage="Snapshot approximate time")]
+        [datetime]$TimeStamp,
+        # Parameter that defines if it's a starting or ending snapshot
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("start","end")]
+        [String]$Mark,
 
         # Switch to turn on the error logging
         [Switch]$ErrorLog,
@@ -309,30 +309,33 @@ function Get-OracleSnapshots {
     Begin{}
     Process{
         if (Check-OracleEnv) {
-            foreach ($db in $TargetDB) {
-                if (podb($db)) {
-					Write-Debug "Database pinged successfully"
-                    $StrStartStamp = $StartTime.AddSeconds(59).ToString("yyyy-MM-dd HH:mm:ss")
-                    Write-Debug $StrStartStamp
-                    $StrEndStamp = $EndTime.ToString("yyyy-MM-dd HH:mm:ss")
-                    Write-Debug $StrEndStamp
-                    # Using here-string to pipe the SQL query to SQL*Plus
-                    @"
+			Write-Debug "Database pinged successfully"
+            if ($Mark -eq "start") {
+                $StrTimeStamp = $TimeStamp.AddSeconds(50).ToString("yyyy-MM-dd HH:mm:ss")
+                Write-Debug $TimeStamp
+                # Using here-string to pipe the SQL query to SQL*Plus
+                @"
 SET HEADING OFF
 SET PAGESIZE 0
 SELECT max(snap_id)
 FROM dba_hist_snapshot
-WHERE begin_interval_time <= TO_TIMESTAMP('$StrStartStamp','YYYY-MM-DD HH24:MI:SS')
-AND end_interval_time > TO_TIMESTAMP('$StrStartStamp','YYYY-MM-DD HH24:MI:SS');
+WHERE begin_interval_time <= TO_TIMESTAMP('$StrTimeStamp','YYYY-MM-DD HH24:MI:SS')
+AND end_interval_time > TO_TIMESTAMP('$StrTimeStamp','YYYY-MM-DD HH24:MI:SS');
+EXIT
+"@ | &"sqlplus" "-S" "/@$TargetDB"
+            } else {
+                $StrTimeStamp = $TimeStamp.ToString("yyyy-MM-dd HH:mm:ss")
+                Write-Debug $TimeStamp
+                # Using here-string to pipe the SQL query to SQL*Plus
+                @"
+SET HEADING OFF
+SET PAGESIZE 0
 SELECT min(snap_id)
 FROM dba_hist_snapshot
-WHERE end_interval_time >= TO_TIMESTAMP('$StrEndStamp','YYYY-MM-DD HH24:MI:SS')
-AND begin_interval_time < TO_TIMESTAMP('$StrEndStamp','YYYY-MM-DD HH24:MI:SS');
+WHERE end_interval_time >= TO_TIMESTAMP('$StrTimeStamp','YYYY-MM-DD HH24:MI:SS')
+AND begin_interval_time < TO_TIMESTAMP('$StrTimeStamp','YYYY-MM-DD HH24:MI:SS');
 EXIT
-"@ | &"sqlplus" "-S" "/@$db"
-                } else {
-                    Write-Error "Database $db not reachable" | Out-File $ErrorLogFile
-                }
+"@ | &"sqlplus" "-S" "/@$TargetDB"
             }
         } else {
             Write-Error "No Oracle Home detected, please install at least the Oracle Client and try again"
@@ -341,7 +344,273 @@ EXIT
     End{}
 }
 
+<#
+.Synopsis
+    
+.DESCRIPTION
+    
+.EXAMPLE
+    
+.ROLE
+    
+#>
+function Get-OracleSnapshotTime {
+    [CmdletBinding()]
+    [Alias("gost")]
+    Param (
+        # This can be a list of databases
+        [Parameter(Mandatory=$true,
+            HelpMessage="Target Oracle Database name")]
+        [String]$TargetDB,
 
+        # This can be a list of databases
+        [Parameter(Mandatory=$true,
+            HelpMessage="Oracle Database ID")]
+        [bigint]$DBID,
+
+        # This is the snapshot to use to query the time
+        [Parameter(Mandatory=$true,
+            HelpMessage="Starting snapshot approximate time")]
+        [bigint]$Snapshot,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("start","end")]
+        [String]$Mark,
+
+        # Switch to turn on the error logging
+        [Switch]$ErrorLog,
+        [String]$ErrorLogFile = "$env:TEMP\OracleUtils_Errors_$PID.log"
+    )
+    Begin{}
+    Process{
+        if (Check-OracleEnv) {
+            # Using here-string to pipe the SQL query to SQL*Plus
+            if ($Mark -eq "start") {
+                @"
+SET HEADING OFF
+SET PAGESIZE 0
+SELECT TO_CHAR(MAX(begin_interval_time),'YYYY-MM-DD HH24:MI:SS')
+FROM dba_hist_snapshot 
+WHERE snap_id = $Snapshot
+AND dbid = $DBID;
+EXIT
+"@ | &"sqlplus" "-S" "/@$TargetDB"
+            } else {
+                @"
+SET HEADING OFF
+SET PAGESIZE 0
+SELECT TO_CHAR(MAX(end_interval_time),'YYYY-MM-DD HH24:MI:SS')
+FROM dba_hist_snapshot 
+WHERE snap_id = $Snapshot
+AND dbid = $DBID;
+EXIT
+"@ | &"sqlplus" "-S" "/@$TargetDB"
+            }
+        } else {
+            Write-Error "No Oracle Home detected, please install at least the Oracle Client and try again"
+        }
+    }
+    End{}
+}
+
+<#
+.Synopsis
+    
+.DESCRIPTION
+    
+.EXAMPLE
+    
+.ROLE
+    
+#>
+function Get-OracleADDMInstanceReport {
+    [CmdletBinding()]
+    [Alias("goaddmi")]
+    Param (
+        # Target Database
+        [Parameter(Mandatory=$true,
+            HelpMessage="Target Oracle Database name")]
+        [String]$TargetDB,
+
+        # Target Database
+        [Parameter(Mandatory=$true,
+            HelpMessage="Target Oracle Instance name")]
+        [String]$Instance,
+
+        # This is the DBID of the target database
+        [Parameter(Mandatory=$true,
+            HelpMessage="Oracle Database ID")]
+        [bigint]$DBID,
+
+
+        # This is the starting snapshot for the Report scope
+        [Parameter(Mandatory=$true,
+            HelpMessage="Starting snapshot approximate time")]
+        [bigint]$StartSnapshot,
+        # This is the ending snapshot for the Report scope
+        [Parameter(Mandatory=$true,
+            HelpMessage="Starting snapshot approximate time")]
+        [bigint]$EndSnapshot,
+
+        # Switch to turn on the error logging
+        [Switch]$ErrorLog,
+        [String]$ErrorLogFile = "$env:TEMP\OracleUtils_Errors_$PID.log"
+    )
+    Begin{}
+    Process{
+        if (Check-OracleEnv) {
+            $InstNumber = $Instance.Substring($Instance.Length-1)
+            # Using here-string to pipe the SQL query to SQL*Plus
+            @"
+define  db_name      = '$TargetDB';
+define  dbid         = $DBID;
+define  inst_num     = $InstNumber;
+define  inst_name    = '$Instance';
+define  num_days     = 3;
+define  begin_snap   = $StartSnapshot;
+define  end_snap     = $EndSnapshot;
+define  report_type  = 'html';
+define  report_name  = 'addm_${Instance}_${StartSnapshot}_${EndSnapshot}_report.html'
+@@?/rdbms/admin/addmrpti.sql
+exit;
+EXIT
+"@ | &"sqlplus" "-S" "/@$TargetDB"
+        } else {
+            Write-Error "No Oracle Home detected, please install at least the Oracle Client and try again"
+        }
+    }
+    End{}
+}
+
+<#
+.Synopsis
+    
+.DESCRIPTION
+    
+.EXAMPLE
+    
+.ROLE
+    
+#>
+function Get-OracleAWRReport {
+    [CmdletBinding()]
+    [Alias("goawrg")]
+    Param (
+        # This can be a list of databases
+        [Parameter(Mandatory=$true,
+            HelpMessage="Target Oracle Database name")]
+        [String]$TargetDB,
+
+        # This can be a list of databases
+        [Parameter(Mandatory=$true,
+            HelpMessage="Oracle Database ID")]
+        [bigint]$DBID,
+
+        # This is the starting snapshot for the Report scope
+        [Parameter(Mandatory=$true,
+            HelpMessage="Starting snapshot approximate time")]
+        [bigint]$StartSnapshot,
+        # This is the ending snapshot for the Report scope
+        [Parameter(Mandatory=$true,
+            HelpMessage="Starting snapshot approximate time")]
+        [bigint]$EndSnapshot,
+
+        # Switch to turn on the error logging
+        [Switch]$ErrorLog,
+        [String]$ErrorLogFile = "$env:TEMP\OracleUtils_Errors_$PID.log"
+    )
+    Begin{}
+    Process{
+        if (Check-OracleEnv) {
+			Write-Verbose "Launching AWR Global Report"
+            # Using here-string to pipe the SQL query to SQL*Plus
+            @"
+define  db_name      = '$TargetDB';
+define  dbid         = $DBID;
+define  num_days     = 3;
+define  begin_snap   = $StartSnapshot;
+define  end_snap     = $EndSnapshot;
+define  report_type  = 'html';
+define  report_name  = 'awr_${TargetDB}_${StartSnapshot}_${EndSnapshot}_global_report.html'
+@@?/rdbms/admin/awrgrpt.sql
+exit;
+EXIT
+"@ | &"sqlplus" "-S" "/@$TargetDB"
+        } else {
+            Write-Error "No Oracle Home detected, please install at least the Oracle Client and try again"
+        }
+    }
+    End{}
+}
+
+<#
+.Synopsis
+    
+.DESCRIPTION
+    
+.EXAMPLE
+    
+.ROLE
+    
+#>
+function Get-OracleAWRInstanceReport {
+    [CmdletBinding()]
+    [Alias("goawri")]
+    Param (
+        # Target Database
+        [Parameter(Mandatory=$true,
+            HelpMessage="Target Oracle Database name")]
+        [String]$TargetDB,
+
+        # Target Database
+        [Parameter(Mandatory=$true,
+            HelpMessage="Target Oracle Instance name")]
+        [String]$Instance,
+
+        # This is the DBID of the target database
+        [Parameter(Mandatory=$true,
+            HelpMessage="Oracle Database ID")]
+        [bigint]$DBID,
+
+
+        # This is the starting snapshot for the Report scope
+        [Parameter(Mandatory=$true,
+            HelpMessage="Starting snapshot approximate time")]
+        [bigint]$StartSnapshot,
+        # This is the ending snapshot for the Report scope
+        [Parameter(Mandatory=$true,
+            HelpMessage="Starting snapshot approximate time")]
+        [bigint]$EndSnapshot,
+
+        # Switch to turn on the error logging
+        [Switch]$ErrorLog,
+        [String]$ErrorLogFile = "$env:TEMP\OracleUtils_Errors_$PID.log"
+    )
+    Begin{}
+    Process{
+        if (Check-OracleEnv) {
+            $InstNumber = $Instance.Substring($Instance.Length-1)
+            # Using here-string to pipe the SQL query to SQL*Plus
+            @"
+define  db_name      = '$TargetDB';
+define  dbid         = $DBID;
+define  inst_num     = $InstNumber;
+define  inst_name    = '$Instance';
+define  num_days     = 3;
+define  begin_snap   = $StartSnapshot;
+define  end_snap     = $EndSnapshot;
+define  report_type  = 'html';
+define  report_name  = 'awr_${Instance}_${StartSnapshot}_${EndSnapshot}_report.html'
+@@?/rdbms/admin/awrrpti.sql
+exit;
+EXIT
+"@ | &"sqlplus" "-S" "/@$TargetDB"
+        } else {
+            Write-Error "No Oracle Home detected, please install at least the Oracle Client and try again"
+        }
+    }
+    End{}
+}
 
 <#
 .Synopsis
@@ -354,37 +623,99 @@ EXIT
        This cmdlet is mean to be used by Oracle DBAs
 #>
 function Get-OraclePerfReport {
-    [CmdletBinding()]
+    [CmdletBinding(
+    SupportsShouldProcess=$true)]
     [Alias("gopr")]
     Param (
         # This can be a list of databases
         [Parameter(Mandatory=$true,
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true,
-            HelpMessage="One or more Oracle Database names")]
-        [String[]]$TargetDB,
-        # This is the starting time for the permoance snapshot
+            HelpMessage="Target Oracle Database names")]
+        [String]$TargetDB,
+        # This is the starting time for the performance snapshot
         [Parameter(Mandatory=$true,
             HelpMessage="Starting snapshot approximate time")]
         [datetime]$StartTime,
-        # This is the ending time for the permoance snapshot
+        # This is the ending time for the performance snapshot
         [Parameter(Mandatory=$true,
             HelpMessage="Ending snapshot approximate time")]
         [datetime]$EndTime,
+        # Switch to turn on ADDM report package
+        [Switch]$ADDM,
+
+        # Switch to turn on AWR report package
+        [Switch]$AWR,
+        # Switch to create compressed report sets
+        [Switch]$Compress,
+        # Switch to send email report sets
+        [Switch]$SendMail,
+        [String[]]$EmailAddress,
         # Switch to turn on the error logging
         [Switch]$ErrorLog,
         [String]$ErrorLogFile = "$env:TEMP\OracleUtils_Errors_$PID.log"
     )
-    Begin {}
+    Begin {
+        if (-not $ADDM -and -not $AWR) {
+            $ADDM = $true
+            $AWR = $true
+        }
+    }
     Process {
-        Write-Warning "NOT IMPLEMENTED YET!"
+        Write-Warning "CURRENTLY UNDER DEVELOPMENT!"
         if (Check-OracleEnv) {
             if (Ping-OracleDB -TargetDB $TargetDB) {
-				Write-Debug "Database pinged successfully"
-				$result = Get-OracleSnapshots -TargetDB $TargetDB -StartTime $StartTime.AddSeconds(59).ToString("yyyy-MM-dd HH:mm:ss") -EndTime $EndTime.ToString("yyyy-MM-dd HH:mm:ss")
-				$StartSnapshot = $result[0]
-				$EndSnapshot = $result[1]
-				Write-Output "Starting Snapshot: $StartSnapshot | Ending Snapshot: $EndSnapshot"
+				Write-Verbose "Database pinged successfully"
+                Write-Output "Gathering DBID"
+                [String]$TempStr = Get-OracleDBID -TargetDB $TargetDB
+                [bigint]$DBID = $TempStr.Trim(' ')
+                Write-Verbose "DBID for $TargetDB : $DBID"
+                Write-Output "Getting Snapshot numbers"
+                [String]$StrSnapshot = Get-OracleSnapshot -TargetDB $TargetDB -TimeStamp $StartTime.AddSeconds(59).ToString("yyyy-MM-dd HH:mm:ss") -Mark start
+                [bigint]$StartSnapshot = [bigint]$StrSnapshot.trim(' ')
+                [String]$StrSnapshot = Get-OracleSnapshot -TargetDB $TargetDB -TimeStamp $EndTime.ToString("yyyy-MM-dd HH:mm:ss") -Mark end
+                [bigint]$EndSnapshot = [bigint]$StrSnapshot.trim(' ')
+				Write-Verbose "Starting Snapshot: $StartSnapshot | Ending Snapshot: $EndSnapshot"
+                Write-Output "Getting Snapshot times"
+                [String]$StartSnapTime = Get-OracleSnapshotTime -TargetDB $TargetDB -DBID $DBID -Snapshot $StartSnapshot -Mark start
+                [String]$EndSnapTime = Get-OracleSnapshotTime -TargetDB $TargetDB -DBID $DBID -Snapshot $EndSnapshot -Mark end
+                Write-Verbose "Starting Snapshot Time: $StartSnapTime | Ending Snapshot Time: $EndSnapTime"
+                if ($AWR) {
+                    Write-Output "Generating AWR Report Set"
+                    $ORAOutput = Get-OracleAWRReport -TargetDB $TargetDB -DBID $DBID -StartSnapshot $StartSnapshot -EndSnapshot $EndSnapshot
+                    Write-Output "Getting Oracle database instances"
+                    $Instances = Get-OracleInstances -TargetDB $TargetDB
+                    foreach ($Instance in $Instances) {
+                        if ($Instance.Length -gt 0) {
+                            Write-Verbose "Launching AWR Instance #$InstNumber Report for $Instance"
+                            $ORAOutput = Get-OracleAWRInstanceReport -TargetDB $TargetDB -Instance $Instance -DBID $DBID -StartSnapshot $StartSnapshot -EndSnapshot $EndSnapshot
+                        }
+                    }
+                }
+                if ($ADDM) {
+                    Write-Output "Generating ADDM Report Set"
+                    foreach ($Instance in $Instances) {
+                        if ($Instance.Length -gt 0) {
+                            Write-Verbose "Launching ADDM Instance #$InstNumber Report for $Instance"
+                            $ORAOutput = Get-OracleADDMInstanceReport -TargetDB $TargetDB -Instance $Instance -DBID $DBID -StartSnapshot $StartSnapshot -EndSnapshot $EndSnapshot
+                        }
+                    }
+                }
+                if ($Compress) {
+                    Write-Output "Compressing AWR reports set"
+                    zip -9m AWR_${TargetDB}_${StartSnapshot}_${EndSnapshot}_reports.zip awr*.htm*
+                    Write-Output "Compressing ADDM reports set"
+                    zip -9m ADDM_${TargetDB}_${StartSnapshot}_${EndSnapshot}_reports.zip addm*.htm*
+                }
+                if ($SendMail) {
+                $EmailAddress = "some_email@some_domain.com"
+                    if ($Compress) {
+                        $ReportFiles = Get-ChildItem -Path . -Name *reports.zip
+                    } else {
+                        $ReportFiles = Get-ChildItem -Path . -Name *report.*
+                    }
+                Send-MailMessage -Attachments $ReportFiles -From $env:USERNAME -To $EmailAddress -Subject "Reports Test" -Body "Some message"
+                }
             }
         }
     }
