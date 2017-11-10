16e6cb3a237f7a453e89bd472b4203e0e6700a48
Jesus Alejandro Sanchez Davila <jsanchez.consultant@gmail.com>
2017-11-10 17:22:40 -0600

    Added directory and draft ps1 for MySQL

---
+++ mysql/MySQLUtils.ps1
@@ -0,0 +1,14 @@
+<#
+.Synopsis
+   Oracle Utilities for using within PowerShell
+.DESCRIPTION
+   This Module has functions to ping Oracle Databases, query them and get performance reports  automatically.
+.EXAMPLE
+   Import-Module \Path\to\MySQLUtils.psm1
+.NOTES
+   General notes
+.ROLE
+   This cmdlet is mean to be used by Oracle DBAs
+.FUNCTIONALITY
+#>
+
