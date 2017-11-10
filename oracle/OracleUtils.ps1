d5d9b55142e989db2275982fb2604568b192be5c
Jesus Sanchez <jsanchez.consultant@gmail.com>
2017-11-10 14:59:20 -0600

    Update OracleUtils.ps1

--- oracle/OracleUtils.ps1
+++ oracle/OracleUtils.ps1
@@ -13,6 +13,270 @@
 #>
 
 
+$OracleHtmlHeader = @'
+<html>
+<head>
+<style type='text/css'>
+body {font:normal 10pt Arial,Helvetica,sans-serif; color:black; background:White;}
+p {font:bold 10pt Arial,Helvetica,sans-serif; color:black; background:White;}
+    table,tr,td {font:bold 10pt Arial,Helvetica,sans-serif; color:Black; background:#f7f7e7;
+    padding:0px 0px 0px 0px; margin:0px 0px 0px 0px;} th {font:bold 10pt Arial,Helvetica,sans-serif;
+    color:blue; background:#cccc99; padding:0px 0px 0px 0px;} h1 {font:16pt Arial,Helvetica,Geneva,sans-serif;
+    color:#336699; background-color:White; border-bottom:1px solid #cccc99; margin-top:0pt; margin-bottom:0pt;
+    padding:0px 0px 0px 0px;} h2 {font:bold 10pt Arial,Helvetica,Geneva,sans-serif; color:#336699;
+    background-color:White; margin-top:4pt; margin-bottom:0pt;} a {font:9pt Arial,Helvetica,sans-serif;
+    color:#663300; background:#ffffff; margin-top:0pt; margin-bottom:0pt;
+    vertical-align:top;}
+</style>
+</head>
+<body>
+'@
+
+$OracleHtmlTail = "</body></html>"
+
+$OracleGeneralHealthCheck = @'
+SET linesize 32000
+SET pagesize 40000
+SET trimspool ON
+SET trimout ON
+SET wrap OFF
+SET FEEDBACK OFF
+SET MARKUP HTML ON
+
+COLUMN title format a100
+COLUMN NAME format a30
+
+SET heading OFF
+SELECT '========== DATABASE INFORMATION ==========' title FROM dual;
+SELECT 'DATABASE NAME: '||db_unique_name FROM v$database;
+SELECT 'NUMBER OF INSTANCES: '||MAX(inst_id) FROM gv$instance;
+SELECT 'DATABASE HOSTS: '||listagg(host_name,', ') WITHIN GROUP(ORDER BY inst_id) FROM gv$instance;
+SELECT 'DATA DISKGROUP/PATH: '||VALUE FROM v$spparameter WHERE upper(NAME)='DB_CREATE_FILE_DEST';
+SELECT 'RECOVERY DISKGROUP/PATH: '||VALUE FROM v$spparameter WHERE upper(NAME)='DB_RECOVERY_FILE_DEST';
+PROMPT
+SET heading OFF
+SELECT '========== SGA INFO ==========' title FROM dual;
+SET heading ON
+COLUMN gb_per_instance FORMAT a30
+SELECT NAME, listagg(round(bytes/1024/1024/1024,2),', ') WITHIN GROUP(ORDER BY inst_id) gb_per_instance 
+FROM gv$sgainfo 
+WHERE upper(NAME) LIKE '%SIZE' 
+AND upper(NAME) NOT LIKE 'GRANULE%' 
+GROUP BY NAME 
+ORDER BY 2 DESC;
+PROMPT
+SET heading OFF
+SELECT '========== CPU INFO ==========' title FROM dual;
+SELECT 'Number of cores/CPUs assigned to instance '||inst_id||': '||VALUE
+FROM gv$spparameter
+WHERE UPPER(name)='CPU_COUNT'
+ORDER BY inst_id;
+SET heading ON
+COLUMN metric_name FORMAT a40
+COLUMN value_per_instance FORMAT a40
+COLUMN metric_unit FORMAT a30
+SELECT metric_name, listagg(ROUND(VALUE,4),' | ') WITHIN GROUP (ORDER BY inst_id) value_per_instance, metric_unit
+FROM gv$sysmetric
+WHERE metric_name LIKE '%CPU%' 
+GROUP BY metric_name, metric_unit
+ORDER BY 2,1;
+PROMPT
+SET heading OFF
+SELECT '========== ASM OPERATIONS ==========' title FROM dual;
+SET heading ON
+SELECT ag.NAME, ao.operation, ao.state, ao.est_minutes FROM v$asm_operation ao, v$asm_diskgroup ag ORDER BY ao.est_minutes;
+PROMPT
+SET heading OFF
+SELECT '========== ASM DISKGROUP STATUS ==========' title FROM dual;
+SET heading ON
+COLUMN "TOTAL GB" format 999,999.99
+COLUMN "FREE GB" format 999,999.99
+COLUMN "USABLE GB" format 999,999.99
+COLUMN "FREE %" format 99.99
+SELECT NAME
+	, offline_disks
+	, state
+	, round(total_mb/1024,2) "TOTAL GB"
+	, round(free_mb/1024,2) "FREE GB"
+	, round(usable_file_mb/1024,2) "USABLE GB"
+	, round(usable_file_mb*100/total_mb,2) "FREE %" 
+FROM v$asm_diskgroup
+WHERE NAME IN (
+	SELECT substr(VALUE,2,LENGTH(VALUE))
+	FROM v$spparameter
+	WHERE upper(NAME) LIKE ('DB_%_FILE_DEST')
+) ORDER BY 1;
+PROMPT
+SET heading OFF
+SELECT '========== FRA STORAGE ==========' title FROM dual;
+SET heading ON
+col name format a32
+col QUOTA_GB for 999,999,999
+col USED_GB for 999,999,999
+col PCT_USED for 999
+SELECT name
+, ceil( space_limit / 1024 / 1024 / 1024) QUOTA_GB
+, ceil( space_used  / 1024 / 1024 / 1024) USED_GB
+, decode( nvl( space_used, 0), 0, 0
+, ceil ( ( space_used / space_limit) * 100) ) PCT_USED
+FROM v$recovery_file_dest
+ORDER BY NAME;
+PROMPT
+SET heading OFF
+SELECT '========== SERVICES ==========' title FROM dual;
+SET heading ON
+COLUMN INSTANCES format a30
+SELECT NAME, listagg(inst_id,',') WITHIN GROUP (ORDER BY inst_id) INSTANCES
+FROM gv$active_services
+WHERE NAME NOT LIKE 'SYS%'
+GROUP BY NAME
+ORDER BY 1;
+PROMPT
+SET heading OFF
+SELECT '========== BLOCKING LOCKS ==========' title FROM dual;
+SET heading ON
+COLUMN "BLOCKER COUNT" format 999999999
+SELECT "BLOCKER COUNT" FROM(
+SELECT count(
+   blocker.SID
+) "BLOCKER COUNT"
+FROM (SELECT *
+      FROM gv$lock
+      WHERE BLOCK != 0
+      AND TYPE = 'TX') blocker
+,    gv$lock            waiting
+WHERE waiting.TYPE='TX' 
+AND waiting.BLOCK = 0
+AND waiting.id1 = blocker.id1
+);
+PROMPT
+SET heading OFF
+SELECT '========== BLOCKING SESSIONS ==========' title FROM dual;
+SET heading ON
+COLUMN username format a16
+SELECT
+   sess.username
+,  sess.osuser
+,  blocker.SID blocker_sid
+,  sess.sql_id
+,  waiting.request
+,  count(waiting.SID) waiting_sid
+,  MAX(trunc(waiting.ctime/60)) max_min_waiting
+FROM (SELECT *
+      FROM gv$lock
+      WHERE BLOCK != 0
+      AND TYPE = 'TX') blocker
+,    gv$lock            waiting
+, gv$session sess
+WHERE waiting.TYPE='TX' 
+AND waiting.BLOCK = 0
+AND waiting.id1 = blocker.id1
+AND sess.SID=blocker.SID
+GROUP BY sess.username, sess.osuser, blocker.SID, sess.sql_id, waiting.request;
+
+SET heading OFF
+SELECT '========== BLOCKING SQLS ==========' title FROM dual;
+SET heading ON
+SELECT DISTINCT sql_id, sql_text
+FROM gv$sql
+WHERE sql_id IN (
+	SELECT
+	   DISTINCT sess.sql_id
+	FROM (SELECT *
+		  FROM gv$lock
+		  WHERE BLOCK != 0
+		  AND TYPE = 'TX') blocker
+	,    gv$lock            waiting
+	, gv$session sess
+	WHERE waiting.TYPE='TX' 
+	AND waiting.BLOCK = 0
+	AND waiting.id1 = blocker.id1
+	AND sess.SID=blocker.SID
+);
+PROMPT
+SET heading OFF
+SELECT '========== LONG DB OPERATIONS ==========' title FROM dual;
+SET heading ON
+SELECT s.username, l.SID, l.serial#, l.opname, l.totalwork, l.sofar, round(l.sofar*100/l.totalwork) percent_complete
+FROM gv$session_longops l, gv$session s
+WHERE totalwork > 0
+AND sofar*100/totalwork < 100
+AND l.SID = s.SID
+AND l.serial# = s.serial#;
+PROMPT
+SET heading OFF
+SELECT '========== PENDING DISTRIBUTED TRANSACTIONS ==========' title FROM dual;
+SET heading ON
+SELECT local_tran_id, state, tran_comment,host,db_user,advice
+FROM dba_2pc_pending
+WHERE state='PREPARED';
+PROMPT
+SET heading OFF
+SELECT '========== PENDING DISTRIBUTED TRANSACTIONS DEATAILS ==========' title FROM dual;
+SET heading ON
+with tran_id as (
+	select local_tran_id
+	from dba_2pc_pending
+	where state='prepared'
+)
+select a.sql_text, s.osuser, s.username
+from v$transaction t, v$session s, v$sqlarea a, tran_id i
+where s.taddr = t.addr
+	and a.address = s.prev_sql_addr
+	and t.xidusn = substr(i.local_tran_id,1,instr(i.local_tran_id,'.')-1)
+	and t.xidslot = substr(i.local_tran_id,(instr(i.local_tran_id,'.')+1),((instr(i.local_tran_id,'.',(instr(i.local_tran_id,'.')+1)))-(instr(i.local_tran_id,'.')+1)))
+	and t.xidsqn = substr(i.local_tran_id,(instr(i.local_tran_id,'.',(instr(i.local_tran_id,'.')+1)))+1,(nvl(nullif(instr(i.local_tran_id,'.',(instr(i.local_tran_id,'.',(instr(i.local_tran_id,'.')+1)))+1),0),length(i.local_tran_id))));
+PROMPT
+SET heading OFF
+SELECT '========== APP/USER SESSIONS ==========' title FROM dual;
+COLUMN "COUNT" format 9999999999999
+COLUMN machine format a30
+SET heading ON
+break ON status
+WITH list_view AS (
+	SELECT status, username, osuser, machine, service_name, inst_id, count(1) session_count
+	FROM gv$session
+	WHERE status='ACTIVE'
+	GROUP BY  inst_id, status, username, osuser, machine, service_name
+	UNION ALL
+	SELECT status, username, osuser, machine, service_name, inst_id, count(1) session_count
+	FROM gv$session
+	WHERE status='INACTIVE'
+	GROUP BY inst_id, status, username, osuser, machine, service_name
+) 
+SELECT status, username, osuser, machine, service_name, listagg('INST: '||inst_id||', COUNT: '||session_count,' | ') WITHIN GROUP (ORDER BY inst_id) "COUNT"
+FROM list_view
+WHERE machine NOT IN (
+	SELECT host_name
+	FROM gv$instance
+)
+GROUP BY status, username, osuser, machine , service_name
+ORDER BY 1,3,2,4;
+PROMPT
+SET heading OFF
+SELECT '========== INTERNAL SESSIONS COUNT ==========' title FROM dual;
+SET heading ON
+SELECT 'ACTIVE SESSIONS' "SESSIONS", count(1) "COUNT"
+FROM gv$session
+WHERE status='ACTIVE'
+AND username IN ('SYS','SYSTEM','DBSNMP')
+UNION ALL
+SELECT 'INACTIVE SESSIONS', count(1) "COUNT"
+FROM gv$session
+WHERE status='INACTIVE'
+AND username IN ('SYS','SYSTEM','DBSNMP');
+PROMPT
+SET heading OFF
+SELECT '========== OUTDATED STATS OBJECTS ==========' title FROM dual;
+SET heading ON
+COLUMN outdated_stats_objects FORMAT 999
+SELECT owner, count(table_name) outdated_stats_objects
+FROM dba_tables
+WHERE last_analyzed < SYSDATE - 7
+GROUP BY owner
+ORDER BY 1;
+'@
+
 <#
 .Synopsis
    Checks that the ORACLE_HOME is set. Returns $true if it is or $false otherwise.
@@ -54,7 +318,7 @@ function Check-OracleEnv {
         <Some commands>
     }
 .ROLE
-   This cmdlet is mean to be used by Oracle DBAs
+   This cmdlet is mean to be used by Oracle 
 .FUNCTIONALITY
 #>
 function Ping-OracleDB
@@ -86,10 +350,11 @@ function Ping-OracleDB
 .ROLE
     This cmdlet is mean to be used by Oracle DBAs
 #>
-function Run-OracleScript {
+function Query-OracleDB {
     [CmdletBinding(
+        DefaultParameterSetName='BySQLQuery',
         SupportsShouldProcess=$true)]
-    [Alias("rsql")]
+    [Alias("qodb")]
     Param (
         # It can run the script on several databases at once
         [Parameter(Mandatory=$true,
@@ -98,10 +363,24 @@ function Run-OracleScript {
             HelpMessage="One or more Oracle Database names")]
         [String[]]$TargetDB,
         # It can run several scripts at once
-        [Parameter(Mandatory=$true,
+        [Parameter(
             ValueFromPipeline=$true,
+            ParameterSetName='BySQLFile',
             HelpMessage="Path to SQL file to run on the databases")]
         [String[]]$SQLScript,
+
+        [Parameter(Mandatory=$true,
+            ValueFromPipeline=$true,
+            ParameterSetName='BySQLQuery',
+            HelpMessage="SQL query to run on the databases")]
+        [String[]]$SQLQuery,
+
+        [Parameter(
+            HelpMessage="Dump results to an output file")]
+        [Switch]$Dump,
+        [Parameter(
+            HelpMessage="Dump results to an output file")]
+        [String]$DumpFile,
 
         # Switch to turn on the error logging
         [Switch]$ErrorLog,
@@ -112,8 +391,26 @@ function Run-OracleScript {
         if (Check-OracleEnv) {
             foreach ($db in $TargetDB) {
                 if (podb($db)) {
-                    Write-Output "Database $db is reachable"
-                    &"sqlplus" "-S" "/@$db" "@$SQLScript"
+                    Write-Verbose "Database $db is reachable"
+                    
+                    if ($PSCmdlet.ParameterSetName -eq 'BySQLFile') {
+                        $Output = &"sqlplus" "-S" "/@$db" "@$SQLScript"
+                    } elseif ($PSCmdlet.ParameterSetName -eq 'BySQLQuery') {
+                        $Output = $SQLQuery | &"sqlplus" "-S" "/@$db"
+                    } else {
+                        Write-Warning "Please use either -SQLFile or -SQLQuery to provide what you need to run on the database"
+                    }
+                    if ($Dump) {
+                        if ($DumpFile.Contains("htm")) {
+                            $OracleHtmlHeader | Out-File $DumpFile
+                        }
+                        $Output | Out-File $DumpFile -Append
+                        if ($DumpFile.Contains("htm")) {
+                            $OracleHtmlTail | Out-File $DumpFile -Append
+                        }
+                    } else {
+                        $Output
+                    }
                 } else {
                     Write-Error "Database $db not reachable"
                 }
@@ -192,8 +489,8 @@ function Get-OracleInstances {
         [Parameter(Mandatory=$true,
             ValueFromPipeline=$true,
             ValueFromPipelineByPropertyName=$true,
-            HelpMessage="One or more Oracle Database names")]
-        [String[]]$TargetDB,
+            HelpMessage="Target Oracle Database name")]
+        [String]$TargetDB,
 
         # Switch to turn on the error logging
         [Switch]$ErrorLog,
@@ -202,20 +499,14 @@ function Get-OracleInstances {
     Begin{}
     Process{
         if (Check-OracleEnv) {
-            foreach ($db in $TargetDB) {
-                if (podb($db)) {
-					Write-Debug "Database pinged successfully"
-                    # Using here-string to pipe the SQL query to SQL*Plus
-                    @'
+			Write-Debug "Database pinged successfully"
+            # Using here-string to pipe the SQL query to SQL*Plus
+            @'
 SET HEADING OFF
 SET PAGESIZE 0
 SELECT instance_name from gv$instance ORDER BY 1;
 exit
-'@ | &"sqlplus" "-S" "/@$db"
-                } else {
-                    Write-Error "Database $db not reachable" | Out-File $ErrorLogFile
-                }
-            }
+'@ | &"sqlplus" "-S" "/@$TargetDB"
         } else {
             Write-Error "No Oracle Home detected, please install at least the Oracle Client and try again"
         }
@@ -470,7 +761,7 @@ define  num_days     = 3;
 define  begin_snap   = $StartSnapshot;
 define  end_snap     = $EndSnapshot;
 define  report_type  = 'html';
-define  report_name  = 'addm_${Instance}_${StartSnapshot}_${EndSnapshot}_report.html'
+define  report_name  = 'addm_${Instance}_${StartSnapshot}_${EndSnapshot}_report.txt'
 @@?/rdbms/admin/addmrpti.sql
 exit;
 EXIT
@@ -622,7 +913,7 @@ EXIT
     .ROLE
        This cmdlet is mean to be used by Oracle DBAs
 #>
-function Get-OraclePerfReport {
+function Get-OraclePerfReports {
     [CmdletBinding(
     SupportsShouldProcess=$true)]
     [Alias("gopr")]
@@ -643,7 +934,6 @@ function Get-OraclePerfReport {
         [datetime]$EndTime,
         # Switch to turn on ADDM report package
         [Switch]$ADDM,
-
         # Switch to turn on AWR report package
         [Switch]$AWR,
         # Switch to create compressed report sets
@@ -662,7 +952,7 @@ function Get-OraclePerfReport {
         }
     }
     Process {
-        Write-Warning "CURRENTLY UNDER DEVELOPMENT!"
+        Write-Output "PS Oracle Performance Reports Generator"
         if (Check-OracleEnv) {
             if (Ping-OracleDB -TargetDB $TargetDB) {
 				Write-Verbose "Database pinged successfully"
@@ -722,7 +1012,7 @@ function Get-OraclePerfReport {
                     }
                     $searcher = [adsisearcher]"(samaccountname=$env:USERNAME)"
                     $FromAddress = $searcher.FindOne().Properties.mail
-                    Send-MailMessage -Attachments $ReportFiles -From $FromAddress -To $FromAddress -Subject "Reports Test" -Body "Some message" -SmtpServer "smtp.wellsfargo.com"
+                    Send-MailMessage -Attachments $ReportFiles -From $FromAddress -To $FromAddress -Subject "Reports Test" -Body "Some message" -SmtpServer "exchange.wellsfargo.com" -Credential "$env:USERDOMAIN\$env:USERNAME" -UseSsl
                 }
             }
         }
