1516c9813d622f04526791fbd3e40ba53243f729
Jesus Sanchez <jsanchez.consultant@gmail.com>
2017-11-20 13:56:23 -0600

    Update MySQLUtils.ps1

--- mysql/MySQLUtils.ps1
+++ mysql/MySQLUtils.ps1
@@ -51,3 +51,28 @@ exit
         $Output
 	}
 }
+
+function Get-MySQLConnection {
+    $UserName = 'username'
+    $Password = 'password'
+    $Database = 'MySQL-DB'
+    $HostName = 'MySQL-Host'
+    $ConnectionString = "server=" + $HostName + ";port=3306;uid=" + $UserName + ";pwd=" + $Password + ";database="+$Database
+
+    Try {
+        [void][System.Reflection.Assembly]::LoadWithPartialName("MySql.Data")
+        $Connection = New-Object MySql.Data.MySqlClient.MySqlConnection
+        $Connection.ConnectionString = $ConnectionString
+        $Connection.Open()
+
+        $Command = New-Object MySql.Data.MySqlClient.MySqlCommand($Query, $Connection)
+        $DataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($Command)
+        $DataSet = New-Object System.Data.DataSet
+        $RecordCount = $dataAdapter.Fill($dataSet, "data")
+        $DataSet.Tables[0]
+    } Catch {
+        Write-Host "ERROR : Unable to run query : $query `n$Error[0]"
+    } Finally {
+        $Connection.Close()
+    }
+}
