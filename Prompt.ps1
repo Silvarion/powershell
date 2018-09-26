26d150053d30c8011335487e5187ce316b3df6f4
Jesus Sanchez <jsanchez.consultant@gmail.com>
2018-09-26 10:34:08 +0200

    Update Prompt.ps1
    Extended git support and last command status 2 lines prompt

--- Prompt.ps1
+++ Prompt.ps1
@@ -1,5 +1,64 @@
-﻿function Prompt {
-$CurrentLocation=$($(Get-Location).ToString() -split '\\')[-1]
-    "[$env:COMPUTERNAME] $CurrentLocation > "
+function prompt {
+    # Grab last exit code
+    $PrevExitCode = $?
+    # Set the ESC Character
+    $ESC = [Char]27
+    # Resolve if in Git Repo
+    if (Get-Command "git.exe" -ErrorAction SilentlyContinue) {
+        $GitStatus = git status
+        # Build Git Section
+        if ($GitStatus) {
+            $GitBranch = $([String]$($GitStatus | Select-String -Pattern "On branch") -split "On branch").Trim(" ")
+            $PendingChanges = [String]$($GitStatus | Select-String -Pattern "Changes not staged")
+            $PendingChangesNumber = $([String]$($GitStatus | Select-String -Pattern "modified|deleted|created") | Measure-Object).Count
+        }
+    }
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = [Security.Principal.WindowsPrincipal] $identity
+    # Debug or Admin
 
+    $IsDebug = $(if (test-path variable:/PSDebugContext) { 'DBG' })
+    if ($IsDebug) {
+        Write-Host "[" -NoNewline
+        Write-Host "DBG" -ForegroundColor 'Yellow' -NoNewline
+        Write-Host "]" -NoNewline
+    }
+    $IsAdmin = $(if($principal.IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) { "ADMIN" })
+    if ($IsAdmin) {
+        Write-Host "[" -NoNewline
+        Write-Host "ADMIN" -ForegroundColor 'Red' -NoNewline
+        Write-Host "]" -NoNewline
+    }
+    $CurrentLocation = $($(Get-Location).ToString() -split '\\')[-1]
+    # Username @ Host section
+    Write-Host "[" -NoNewline
+    Write-Host "$env:USERNAME" -ForegroundColor Cyan -NoNewline
+    Write-Host "@" -NoNewline
+    Write-Host "$env:COMPUTERNAME" -ForegroundColor Yellow -NoNewline
+    Write-Host "] " -NoNewline
+    # Git Status
+    if ($GitStatus) {
+        if ($PendingChanges) {
+            $GitColor = 'Yellow'
+        } else {
+            $GitColor = 'Green'
+        }
+        Write-Host " {$GitBranch | " -NoNewline
+        Write-Host "+-$PendingChangesNumber" -ForegroundColor $GitColor -NoNewline
+        Write-Host " }" -NoNewline
+    }
+    # Last Command exit status
+    if ($PrevExitCode) {
+        Write-Host "(" -NoNewline
+        Write-Host "$([Char]8730)" -ForegroundColor Green -OutVariable $LastCommandExit -NoNewline
+        Write-Host ")" -NoNewline
+    } else {
+        Write-Host "(" -NoNewline
+        Write-Host "$([Char]0215)" -ForegroundColor Red -OutVariable $LastCommandExit -NoNewline
+        Write-Host ")" -NoNewline
+    }
+    # Current Location
+    Write-Host "`n$CurrentLocation -" -NoNewline
+    Write-Host $($(if ($nestedpromptlevel -ge 1) { '>>' }) + '>') -NoNewline
+    return " "
 }
